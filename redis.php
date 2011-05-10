3886e4ce3ab42e7596f85f421c9a417696a7c324
Lloyd Moore <manchesterboy@gmail.com>
2011-05-10 10:56:03 +0200

    update

--- redis.php
+++ redis.php
@@ -8,38 +8,16 @@ class Redis {
 	public function __construct($host='localhost', $port = 6379) {
 		$this->host   = $host;
 		$this->port   = $port;
-		session_write_close();
-		$this->socket = fsockopen($this->host, $this->port, $errno, $errstr);
-		if (!$this->socket) {
+		if (!$this->socket = fsockopen($this->host, $this->port, $errno, $errstr)) {
 			throw new Exception("{$errno} - {$errstr}");
 		}
-		//stream_set_blocking($this->socket, 0);
 	}
 
 	public function __destruct() {
 		fclose($this->socket);
 	}
 
-	/*
-	 * The new unified request protocol as of 1.2 is of the general form:
-	 * '*'<number of args> CR LF
-	 * '$'<length of argument 1> CR LF
-	 * <argument 1> CR LF
-	 * ...
-	 * ...
-	 * <length of argument n> CR LF
-	 * <argument n data>
-	 *
-	 * Example: SET xyz fooball becomes ->
-	 * '*'3\r\n$3\r\nSET\r\n$3\r\nxyzr\nfooball\r\n 
-	 */
-
 	public function __call($name, $args) {
-		/*
-		if(is_array($args[count($args)-1])) {
-			$args = $this->expandArgs($args);
-		}
-		*/
 		array_unshift($args, strtoupper($name));
 		$cmd    = sprintf('*%d%s%s%s', count($args), NL, $this->argsWithLengths($args), NL);
 		$done   = 0;
@@ -51,9 +29,9 @@ class Redis {
 			}
 		}
 
+		$this->chunkSize = 2;
 		$response = $this->handleReply();
 		return $response;
-
 	}
 
 	private function handleReply(){
@@ -81,7 +59,7 @@ class Redis {
 	}
 
 	private function replyError($reply){
-		throw new RedisException(substr(trim($reply), 4));
+		throw new Exception(substr(trim($reply), 4));
 	}
 
 	private function replyInt($reply){
@@ -94,7 +72,7 @@ class Redis {
 		if ($reply == '$-1') {
 			break;
 		}
-		$response = $this->readSocketStream();
+		$response = $this->readSocketStream($reply);
 		return $response;
 	}
 
@@ -105,13 +83,13 @@ class Redis {
 		}
 		$response = array();
 		for ($i = 0; $i < $count; $i++) {
-			$bulk_head = trim(fgets($this->socket, 512));
-			$size      = substr($bulk_head, 1);
+			$read = trim(fgets($this->socket, 512));
+			$size = substr($read, 1);
 			$response[] = ($size == '-1') 
 				? null 
-				: $this->readSocketStream(); 
+				: $this->readSocketStream($read); 
 		}
-		return $reply;
+		return $this->asHashArray($response);
 	}
 
 	/*******************
@@ -119,38 +97,36 @@ class Redis {
 	 *******************/
 	private function argsWithLengths($args){
 		$cmd_parts = array_map(function($x){ 
-			return sprintf('$%d\r\n%s\r\n', strlen($x), $x); }, 
+			return sprintf("$%d%s%s", strlen($x), NL, $x); }, 
 			$args
 		);
-		return implode('\r\n', $cmd_parts);
+		return implode(NL, $cmd_parts).NL;
 	}
 
-	private function expandArgs($args){
-		$first = $args[0];
-		$rev   = array_reverse($args);
+	private function asHashArray(array $values){
+		$pairs = array_chunk($values, 2);
 		$out   = array();
-		foreach($rev[0] as $k => $v){
-			$out[] = $k;
-			$out[] = $v;
+		foreach ($pairs as $pair){
+			$out[$pair[0]] = $pair[1];
 		}
-		array_unshift($out, $args[0]);
 		return $out;
 	}
 
-	private function readSocketStream(){
-		$so_far = 0;
-		$block = "";
+	private function inChunks($array){
+		return array_chunk($array, $this->getChunkSize());
+	}
+
+	private function readSocketStream($read, $so_far=0){
+		$response = "";
+		$size  = substr($read, 1);
 		do {
-			$block  .= fread($this->socket, min($size - $so_far));
-			echo $block;
-			$so_far += $read_ptr;
+			$amt_to_read = min(1024, ($size - $so_far));
+			$response   .= fread($this->socket, $amt_to_read);
+			$so_far     += $amt_to_read;
 		} while ($so_far < $size);
 		fread($this->socket, 2); 
-		return $block;
+		return $response;
 	}
 
 }
 
-$r = new Redis();
-$r->info();
-$r->set('testme', 'foobar');
