bc3799f7b13a748f6656079a2c4e01b12e5671d1
lloyd <manchesterboy@gmail.com>
2014-09-07 19:34:55 +0200

    update

--- redis.php
+++ redis.php
@@ -1,11 +1,21 @@
 <?php
-define('NL', sprintf('%s%s', chr(13), chr(10)));
-class Redis {
-	private $socket;
-	public $host;
-	public $port;
 
-	public function __construct($host='localhost', $port = 6379) {
+define('NL', sprintf('%s%s', chr(13), chr(10)));
+
+/**
+ * Class Rdis
+ */
+class Rdis {
+
+	protected $socket;
+	protected $host;
+	protected $port;
+
+    /**
+     * @param string $host
+     * @param int $port
+     */
+    public function __construct($host='localhost', $port = 6379) {
 		$this->host   = $host;
 		$this->port   = $port;
 		if (!$this->socket = fsockopen($this->host, $this->port, $errno, $errstr)) {
@@ -13,21 +23,24 @@ class Redis {
 		}
 	}
 
-	public function __destruct() {
+    /**
+     *
+     */
+    public function __destruct() {
 		fclose($this->socket);
 	}
 
-	/**
-	 * Todos la llamadas de redis entran aqui. Para ver un explicacion como funciona
-	 * __call mira el docs in http://www.php.net
-
-	 */
-	public function __call($name, $args) {
+    /**
+     * @param $name
+     * @param $args
+     * @return mixed
+     * @throws Exception
+     */
+    public function __call($name, $args) {
 		array_unshift($args, strtoupper($name));
-		$cmd    = sprintf('*%d%s%s%s', count($args), NL, $this->argsWithLengths($args), NL);
-		$done   = 0;
-		$cmdlen = strlen($cmd);
-		for ($w = 0; $w < $cmdlen; $w += $done) {
+		$cmd = sprintf('*%d%s%s%s', count($args), NL, $this->argsWithLengths($args), NL);
+		$commandLength = strlen($cmd);
+		for ($w = 0; $w < $commandLength; $w += $done) {
 			$done = fwrite($this->socket, substr($cmd, $w));
 			if ($done === FALSE) {
 				throw new Exception('Failed to write entire command to stream');
@@ -38,7 +51,10 @@ class Redis {
 		return $response;
 	}
 
-	private function handleReply(){
+    /**
+     * @return mixed
+     */
+    protected function handleReply(){
 		$reply = trim(fgets($this->socket, 512));
 		$responses = array(
 			'+' => 'replySingle' , '-' => 'replyError'     , ':' => 'replyInt' , 
@@ -46,41 +62,38 @@ class Redis {
 		);
 		$response_code = substr($reply, 0, 1);
 		try {
-			$func = array($this, $responses[$response_code]);
-			$response = call_user_func_array($func, array($reply));
+			$func = [$this, $responses[$response_code]];
+			$response = call_user_func_array($func, [$reply]);
 			return $response;
 		} catch (Exception $e) {
 			die("server response makes no sense to me: {$reply}");
 		}
 	}
 
-	/******************
-	 * Reply Handlers *
-	 ******************/
-	private function replySingle($reply){
+	protected function replySingle($reply){
 		$response = substr(trim($reply), 1);
 		return $response;
 	}
 
-	private function replyError($reply){
+	protected function replyError($reply){
 		throw new Exception(substr(trim($reply), 4));
 	}
 
-	private function replyInt($reply){
+	protected function replyInt($reply){
 		$response = intval(substr(trim($reply), 1));
 		return $response;
 	}
 
-	private function replyBulk($reply){
+	protected function replyBulk($reply){
 		$response = null;
 		if ($reply == '$-1') {
-			break;
+			return null;
 		}
 		$response = $this->readSocketStream($reply);
 		return $response;
 	}
 
-	private function replyMultiBulk($reply){
+	protected function replyMultiBulk($reply){
 		$count = substr($reply, 1);
 		if ($count == '-1') {
 			return null;
@@ -99,7 +112,12 @@ class Redis {
 	/*******************
 	 * Utility methods *
 	 *******************/
-	private function argsWithLengths($args){
+
+    /**
+     * @param $args
+     * @return string
+     */
+    protected function argsWithLengths($args){
 		$cmd_parts = array_map(function($x){ 
 			return sprintf("$%d%s%s", strlen($x), NL, $x); }, 
 			$args
@@ -107,7 +125,11 @@ class Redis {
 		return implode(NL, $cmd_parts).NL;
 	}
 
-	private function asHashArray(array $values){
+    /**
+     * @param array $values
+     * @return array
+     */
+    protected function asHashArray(array $values){
 		$pairs = array_chunk($values, 2);
 		$out   = array();
 		foreach ($pairs as $pair){
@@ -116,11 +138,12 @@ class Redis {
 		return $out;
 	}
 
-	private function inChunks($array){
-		return array_chunk($array, $this->getChunkSize());
-	}
-
-	private function readSocketStream($read, $so_far=0){
+    /**
+     * @param $read
+     * @param int $so_far
+     * @return string
+     */
+    protected function readSocketStream($read, $so_far=0){
 		$response = "";
 		$size  = substr($read, 1);
 		do {
--- test_redis.php
+++ test_redis.php
@@ -1,6 +1,11 @@
 <?php
+
 include 'redis.php';
-$r = new Redis('localhost', 6379);
+
+$r = new Rdis('localhost', 6379);
+
+/** @var $r Re*/
 $r->hmset('myhmset', 'foo', 'yep', 'bar', 'pey');
 $r->lpush('mylist', 'foobar');
-$r->hgetall('myhmset');
+print_r($r->hgetall('myhmset'));
+
